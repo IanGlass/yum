c5cd975f08492896dec62658a2feab074ac2eaa8
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-08-06 09:53:05 +1200

    Use Material bottom tab on android

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+
 import CatergoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
@@ -24,7 +26,7 @@ const MealsNavigator = createStackNavigator({
   }
 });
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+const tabScreenConfig = {
   Meals: {
     screen: MealsNavigator,
     navigationOptions: {
@@ -34,25 +36,35 @@ const MealsFavTabNavigator = createBottomTabNavigator({
           size={25}
           color={tabInfo.tintColor}
         />
-      )
+      ),
+      tabBarColor: Colors.primary
     },
   },
   Favourites: {
     screen: FavouritesScreen,
     navigationOptions: {
       tabBarIcon: (tabInfo) => (
-        <Ionicons 
+        <Ionicons
           name='ios-star'
           size={25}
           color={tabInfo.tintColor}
         />
-      )
+      ),
+      tabBarColor: Colors.accent
     }
   }
-}, {
-  tabBarOptions: {
-    activeTintColor: Colors.accent
-  }
-});
+};
+
+const MealsFavTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+      activeTintColor: Colors.accent,
+      shifting: true
+    })
+    : createBottomTabNavigator(tabScreenConfig, {
+      tabBarOptions: {
+        activeTintColor: Colors.accent
+      }
+    });
 
 export default createAppContainer(MealsFavTabNavigator);
