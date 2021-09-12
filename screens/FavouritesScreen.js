6396be0a9ad79b151f23370762f1cf6939263fb8
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-09-12 13:07:32 +1200

    Add fallback text for empty filters

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const FavouritesScreen = ({ navigation }) => {
   const favMeals = useSelector((state) => state.meals.favouriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favourite meals found. Start adding some!</DefaultText>
+      </View>
+    )
+  }
 
   return (
     <MealList
@@ -27,5 +37,13 @@ FavouritesScreen.navigationOptions = (navigationData) => {
     )
   }
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavouritesScreen;
